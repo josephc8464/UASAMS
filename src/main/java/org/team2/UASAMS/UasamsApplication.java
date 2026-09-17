0644b24eada61bdbfbe62a707905e7b2a26fc2f9
Joseph Corella <josephc8464@gmail.com>
2026-09-17 12:21:32 -0700

    Initial Spring Boot Setup

---
+++ src/main/java/org/team2/UASAMS/UasamsApplication.java
@@ -0,0 +1,13 @@
+package org.team2.UASAMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UasamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UasamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/team2/UASAMS/UasamsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team2.UASAMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UasamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
